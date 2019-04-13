d853c150641493139f29c0e7ed6379dd8312f092
aretecode <aretecode@gmail.com>
2019-04-13 15:56:54 +0530

    add globals.d.ts

---
+++ globals.d.ts
@@ -0,0 +1,35 @@
+// if we import this, it fails
+// import { NormalizedCacheObject } from 'apollo-boost'
+
+interface NormalizedCacheObject {
+  [key: string]: unknown
+}
+
+declare global {
+  interface Window {
+    readonly __LAST_PUBLISH_TIMESTAMP__: number
+    readonly __APOLLO_STATE__: NormalizedCacheObject
+    readonly __APP_STATE__: { [key: string]: unknown }
+    readonly __ROUTER_STATE__: { [key: string]: unknown }
+  }
+}
+
+interface Window {
+  readonly __LAST_PUBLISH_TIMESTAMP__: number
+  readonly __APOLLO_STATE__: { [key: string]: unknown }
+  readonly __APP_STATE__: { [key: string]: unknown }
+  readonly __ROUTER_STATE__: { [key: string]: unknown }
+}
+
+declare namespace NodeJS {
+  interface Global {
+    process: Process
+  }
+  interface Process {
+    browser: boolean
+
+    env: {
+      NODE_ENV: 'test' | 'development' | 'production'
+    }
+  }
+}
