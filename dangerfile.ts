d7147dcc08a3f85654d7f4ec451df12de358e3a9
aretecode <aretecode@gmail.com>
2019-04-23 22:45:30 -0700

    dangerfile comment links

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,3 +1,8 @@
+/**
+ * @api @see https://danger.systems/js/
+ *
+ * @example https://github.com/apollographql/apollo-client/blob/master/config/dangerfile.ts
+ */
 import { message, danger } from 'danger'
 
 const modifiedMD = danger.git.modified_files.join('- ')
