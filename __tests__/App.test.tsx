657522cfbe4d4830ef259032b4badc816ed55dc9
aretecode <aretecode@gmail.com>
2019-04-23 19:49:57 -0700

    ! app snapshots running in AmpContext

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,6 +7,7 @@ import { defaultApolloStatePortfolio } from '../src/apolloState'
 import { InnerApp } from '../pages/_app'
 import AboutPage from '../pages/About'
 import PortfolioPage from '../pages/Portfolio'
+import { AmpContext } from '../src/features/AmpContext'
 
 const sleep = async (time: number) => Promise.resolve(time)
 
@@ -17,6 +18,27 @@ describe('app', () => {
         <PortfolioPage />
         <AboutPage />
       </InnerApp>
+    )
+    const { container, rerender, getByText } = render(view)
+    rerender(view)
+    console.log('[tests] App - waiting')
+    await waitForElement(() =>
+      getByText(defaultApolloStatePortfolio.work[0].company)
+    )
+    await sleep(5000)
+
+    console.log('[tests] App - waited')
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should match snapshot for amp', async () => {
+    const view = (
+      <AmpContext.Provider value={{ isAmp: true }}>
+        <InnerApp>
+          <PortfolioPage />
+          <AboutPage />
+        </InnerApp>
+      </AmpContext.Provider>
     )
     const { container, rerender, getByText } = render(view)
     rerender(view)
