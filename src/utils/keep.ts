bbee1fd710515bcd3e4c6a5ce0b53b14f3a48f92
James <aretecode@gmail.com>
2021-03-09 00:09:10 -0800

    feat: strongly types keep util

--- src/utils/keep.ts
+++ src/utils/keep.ts
@@ -3,20 +3,18 @@
  */
 import { AnyObj } from '../typings'
 
-export type StringList = string[] | ReadonlyArray<string>
-
-/**
- * @todo typings for return like omit
- */
-export const keep = <Obj = AnyObj, List extends StringList = StringList>(
+export const keep = <
+  Obj extends {} = AnyObj,
+  List extends Array<keyof Obj> | ReadonlyArray<keyof Obj> = Array<keyof Obj>
+>(
   obj: Obj,
   keys: List
-) => {
-  const result = {}
+): Pick<Obj, List[number]> => {
+  const result = {} as any
   Object.keys(obj)
-    .filter(key => keys.includes(key))
+    .filter(key => keys.includes(key as any))
     .forEach(key => {
       result[key] = obj[key]
     })
-  return result as any
+  return result
 }
