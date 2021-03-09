8d00aef5d10cfad9c0a49ae5bf8c2deb3c6ffb36
James <aretecode@gmail.com>
2021-03-09 00:08:28 -0800

    refactor: vector filter from class to fn

--- src/features/VectorFilter.tsx
+++ src/features/VectorFilter.tsx
@@ -4,25 +4,27 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-export const VectorFilter = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg {...props}>
-    <filter
-      id="green-tint"
-      colorInterpolationFilters="sRGB"
-      x="0"
-      y="0"
-      height="100%"
-      width="100%"
-    >
-      <feColorMatrix
-        type="matrix"
-        values="0.36 0 0 0  0.02
+export const VectorFilter: React.FC<React.SVGProps<SVGSVGElement>> = React.memo(
+  props => (
+    <svg {...props}>
+      <filter
+        id="green-tint"
+        colorInterpolationFilters="sRGB"
+        x="0"
+        y="0"
+        height="100%"
+        width="100%"
+      >
+        <feColorMatrix
+          type="matrix"
+          values="0.36 0 0 0  0.02
               0.56 0 0 0  0.06
               0.40 0 0 0  0.16
                 0  0 0 1  0"
-      />
-    </filter>
-  </svg>
+        />
+      </filter>
+    </svg>
+  )
 )
 
 /**
