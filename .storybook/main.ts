56885be2c9a4468b1fc664aca1e942fc13ee5635
James <aretecode@gmail.com>
2021-03-08 23:53:22 -0800

    refactor: migrate to newer storybook config

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/react/types';
+
+module.exports = {
+  stories: ['./src/**/*.stories.*'],
+  logLevel: 'debug',
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+    '@storybook/addon-graphql',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          parser: 'typescript',
+          injectStoryParameters: false,
+        },
+      },
+    },
+    '@storybook/addon-storyshots',
+  ],
+  typescript: {
+    check: true,
+    checkOptions: {},
+    reactDocgenTypescriptOptions: {
+      propFilter: (prop) => ['label', 'disabled'].includes(prop.name),
+    },
+  },
+} as StorybookConfig;
