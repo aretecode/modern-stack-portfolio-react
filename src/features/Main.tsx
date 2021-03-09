714456276a4b0a5258260fe61fc520034cf9d44c
James <aretecode@gmail.com>
2021-03-09 00:06:17 -0800

    fix: about me page sticky footer

--- src/features/Main.tsx
+++ src/features/Main.tsx
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
-export const StyledMain = styled.main``
+/** height & flex are to make container large enough to push footer to the bottom */
+export const StyledMain = styled.main`
+  min-height: 100%;
+  flex-grow: 1;
+`
