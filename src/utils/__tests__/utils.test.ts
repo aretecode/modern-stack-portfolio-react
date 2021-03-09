80e7935f35a4afec74f795e369128e20192977fe
James <aretecode@gmail.com>
2021-03-09 00:08:47 -0800

    test: remove old image tests

--- src/utils/__tests__/utils.test.ts
+++ src/utils/__tests__/utils.test.ts
@@ -1,23 +1,11 @@
 /**
  * @note - have not tested `is`, it is tested in `chain-able`
  */
-import { addTypeName } from '../addTypeName'
 import { requestIdleCallback } from '../requestIdleCallback'
 import { EMPTY_OBJ, EMPTY_ARRAY } from '../EMPTY'
 import { keep } from '../keep'
-import { measureImage } from '../measureImage'
 
 describe('utils', () => {
-  /**
-   * @todo also test browser only does not exist in generated code
-   */
-  describe('measureImage', () => {
-    it('should get the image dimensions correctly', async () => {
-      const src = `https://noccumpr-cdn.sirv.com/images/full-james-wiens-profile-picture.png?format=webp`
-      const dimensions = await measureImage(src)
-      expect(dimensions!.width).toEqual(601)
-    })
-  })
   describe('keep', () => {
     it('should keep props we want, side effect free', () => {
       const KEEP_LIST = Object.freeze(['src', 'alt'])
@@ -56,31 +44,7 @@ describe('utils', () => {
       expectKept(kept)
     })
   })
-  describe('addTypeName', () => {
-    it('should return the same value for all basic types', () => {
-      const list = ['string', 0, () => {}, undefined]
-      const listWithTypeNames = list.map(item => addTypeName('nope', item))
-      expect(listWithTypeNames).toEqual(list)
-    })
-    it('should return undefined for null', () => {
-      expect(addTypeName('', null)).toEqual(undefined)
-    })
-    it('should work on objects, side effect free', () => {
-      const obj = {}
-      const result = addTypeName('eh', obj)
-      expect(obj === result).toEqual(false)
-      expect(result.__typename).toEqual('eh')
-    })
-    it('should for arrays', () => {
-      const list = [
-        {
-          value: 'eh',
-        },
-      ]
-      const result = addTypeName('eh', list)
-      expect(result[0].__typename).toEqual('eh')
-    })
-  })
+
   describe('requestIdleCallback', () => {
     it('should be ~immediate in test environment', () => {
       const now = Date.now()
